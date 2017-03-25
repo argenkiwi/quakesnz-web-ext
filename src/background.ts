74cf892547fa32811190148510465bcbff0c86ac
Leandro Peralta <gringoperalta@gmail.com>
2017-03-25 19:51:08 +1300

    Remove unnecessary curly braces.

--- src/background.ts
+++ src/background.ts
@@ -3,26 +3,24 @@ import { Feature } from "./model/Feature";
 import { fetchQuakes } from "./fetch";
 import { mmiToColour } from "./utils";
 
-chrome.alarms.onAlarm.addListener(() => {
-    chrome.storage.sync.get({
-        intensity: 3,
-        latest: Date.now(),
-    }, (options: Options) => fetchQuakes(options.intensity, (features) => {
-        if (features.length > 0) {
-            let count = 0;
-            let maxMMI = 0;
-            for (let feature of features) {
-                if (options.latest < Date.parse(feature.properties.time)) {
-                    count++;
-                    maxMMI = Math.max(maxMMI, feature.properties.mmi);
-                } else break;
-            }
-            console.log(count + " new quakes reported.");
-            chrome.browserAction.setBadgeText({ text: count > 0 ? count.toString() : "" });
-            chrome.browserAction.setBadgeBackgroundColor({ color: mmiToColour(maxMMI) });
+chrome.alarms.onAlarm.addListener(() => chrome.storage.sync.get({
+    intensity: 3,
+    latest: Date.now(),
+}, (options: Options) => fetchQuakes(options.intensity, (features) => {
+    if (features.length > 0) {
+        let count = 0;
+        let maxMMI = 0;
+        for (let feature of features) {
+            if (options.latest < Date.parse(feature.properties.time)) {
+                count++;
+                maxMMI = Math.max(maxMMI, feature.properties.mmi);
+            } else break;
         }
-    }, error => console.log(error)));
-});
+        console.log(count + " new quakes reported.");
+        chrome.browserAction.setBadgeText({ text: count > 0 ? count.toString() : "" });
+        chrome.browserAction.setBadgeBackgroundColor({ color: mmiToColour(maxMMI) });
+    }
+}, error => console.log(error))));
 
 chrome.runtime.onInstalled.addListener(() => chrome.alarms.create("quakes-nz", {
     delayInMinutes: 1,
