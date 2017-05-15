2b4435bb82937b992f4d0207469926876ee8fb8e
Leandro Peralta <gringoperalta@gmail.com>
2017-05-16 08:10:18 +1200

    Use fetch instead of xhr.

--- src/fetch.ts
+++ src/fetch.ts
@@ -3,17 +3,20 @@ import { Feature } from "./model/Feature";
 export function fetchQuakes(mmi: number,
     onSuccess: (features: Feature[]) => void,
     onError?: (error: string) => void) {
-    let req = new XMLHttpRequest();
-    req.open('GET', "https://api.geonet.org.nz/quake?MMI=" + mmi);
-    req.onload = () => {
-        if (req.status === 200) {
-            let features = JSON.parse(req.responseText).features;
-            if (features.length) chrome.storage.sync.set({
-                latest: Date.parse(features[0].properties.time)
+    window.fetch('https://api.geonet.org.nz/quake?MMI=' + mmi)
+        .then(req => {
+            if (req.status !== 200) {
+                if (onError) onError(req.statusText);
+                return;
+            }
+
+            req.json().then(data => {
+                let features = data.features;
+                if (features.length) chrome.storage.sync.set({
+                    latest: Date.parse(features[0].properties.time)
+                });
+                onSuccess(features);
             });
-            onSuccess(features);
-        } else if (onError) onError(req.statusText);
-    }
-    req.onerror = () => onError("Failed to retrieve quake reports.");;
-    req.send();
+        })
+        .catch(() => onError("Failed to retrieve quake reports."));
 }
